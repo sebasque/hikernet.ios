b8c68427bff48bf6feb367f440c65ce5cf990cfc
Michael Koohang <michael.s.k1001@gmail.com>
2021-04-01 16:48:01 -0400

    Add terms and conditions to settings

--- HikerNet/Views/Main/Settings/SettingsView.swift
+++ HikerNet/Views/Main/Settings/SettingsView.swift
@@ -2,8 +2,9 @@
 import SwiftUI
 
 struct SettingsView: View {
-    @State private var showBrowser = false
-    @State private var url = URL(string: "\(Constants.apiUrl)")!
+    @State private var showAbout = false
+    @State private var showPrivacy = false
+    @State private var showTerms = false
     @State private var cachedHikes = DatabaseManager.getHikes()
     @State private var showNotification: Bool = false
     @State private var notificationTitle: String = "ID Copied"
@@ -14,22 +15,29 @@ struct SettingsView: View {
             Text("Settings")
                 .foregroundColor(.primary)
                 .font(Font.custom(Constants.Fonts.medium, size: 28))
-            SettingsButton(title: "About", image: "chevron.right") {
-                url = URL(string: "\(Constants.apiUrl)")!
-                showBrowser = true
+            SettingsButton(title: "About", image: "chevron.right") { showAbout = true }
+            .sheet(isPresented: $showAbout) {
+                SafariView(url: URL(string: "\(Constants.apiUrl)")!)
             }
-            SettingsButton(title: "Privacy", image: "chevron.right") {
-                url = URL(string: "\(Constants.apiUrl)/#/privacy")!
-                showBrowser = true
+            
+            SettingsButton(title: "Privacy", image: "chevron.right") { showPrivacy = true }
+            .sheet(isPresented: $showPrivacy) {
+                SafariView(url: URL(string: "\(Constants.apiUrl)/#/privacy")!)
             }
+            
+            SettingsButton(title: "Terms and Conditions", image: "chevron.right") { showTerms = true }
+            .sheet(isPresented: $showTerms) {
+                SafariView(url: URL(string: "\(Constants.apiUrl)/#/terms")!)
+            }
+            
             SettingsButton(title: "Your ID: \(UserDefaultsManager.getId())", image: "doc.on.doc") {
                 UIPasteboard.general.string = UserDefaultsManager.getId()
                 haptics.impactOccurred()
                 notificationTitle = "ID Copied"
                 withAnimation { showNotification = true }
-                Timer.scheduledTimer(withTimeInterval: 2, repeats: false) { _ in
+                DispatchQueue.main.asyncAfter(deadline: .now() + 2, execute: {
                     withAnimation { showNotification = false }
-                }
+                })
             }
             if cachedHikes.count > 0 {
                 SettingsButton(title: "\(cachedHikes.count) hike(s) cached", image: "square.and.arrow.down") {
@@ -67,9 +75,6 @@ struct SettingsView: View {
         }
         .padding(EdgeInsets(top: 24, leading: 24, bottom: 24, trailing: 24))
         .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .leading)
-        .sheet(isPresented: $showBrowser, content: {
-            SafariView(url: url)
-        })
         .onAppear() {
             cachedHikes = DatabaseManager.getHikes()
         }
